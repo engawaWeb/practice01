e253be8278afffee070882ad5a992f1c2414118a
engawaWeb <engawa.web@gmail.com>
2019-08-23 12:42:22 +0900

    2019/08/23 01

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?
+echo 'phpのテストです。<br>';
+$dbinfo = parse_url(getenv('DATABASE_URL'));
+$dsn = 'pgsql:host=' . $dbinfo['host'] . ';dbname=' . substr($dbinfo['path'], 1);
+$pdo = new PDO($dsn, $dbinfo['user'], $dbinfo['pass']);
+var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
+phpinfo();
+?>
+</body>
+</html>
